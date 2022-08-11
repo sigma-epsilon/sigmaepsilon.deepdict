fc8646fd61912f2b1f9a9c541ad9db3e266c5787
Bence Balogh <benceeok@gmail.com>
2022-08-11 19:37:30 +0200

    new

---
+++ src/ldd/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from .ldd import LinkedDeepDict
+#from .deepdict import DeepDict as Library
+from .ldd import LinkedDeepDict as DeepDict
+
+__version__ = "0.0.1a"
+
+__description__ = "Linked Deep Dictionaries in Python."
---
+++ src/ldd/tools/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+from .kwargtools import *
+from .dtk import *
