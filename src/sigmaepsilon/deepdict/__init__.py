ec3a16b52a32fa1509958183ad84fc75e793ca3a
Bence Balogh <benceeok@gmail.com>
2023-09-30 14:13:22 +0200

    added shortcuts to utils

--- src/sigmaepsilon/deepdict/__init__.py
+++ src/sigmaepsilon/deepdict/__init__.py
@@ -4,8 +4,16 @@ from importlib.metadata import metadata
 from sigmaepsilon.core.config import namespace_package_name
 
 from .deepdict import DeepDict
+from .utils import dictparser, parseaddress, parsedicts, parseitems, parsedicts_addr
 
-__all__ = ["DeepDict"]
+__all__ = [
+    "DeepDict",
+    "dictparser",
+    "parseaddress",
+    "parseitems",
+    "parsedicts",
+    "parsedicts_addr",
+]
 
 __pkg_name__ = namespace_package_name(dirname(abspath(__file__)), 10)
 __pkg_metadata__ = metadata(__pkg_name__)
